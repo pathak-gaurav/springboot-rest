3340c7a3a37d2d0f70c1da691a0ac6ffed14af87
gauravpathak <gauravpathak.spring@gmail.com>
2020-01-24 22:17:16 +0530

    Initial commit

---
+++ src/test/java/com/gaurav/SpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
